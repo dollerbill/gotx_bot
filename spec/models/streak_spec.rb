5971819f14b7caf2c1c7aed6c1b9335d1dc4c7ed
Billy Cropley <billycropley@gmail.com>
2024-01-07 21:17:48 -0800

    Finish Streak specs

--- spec/models/streak_spec.rb
+++ spec/models/streak_spec.rb
@@ -7,25 +7,26 @@ RSpec.describe Streak, type: :model do
     it { is_expected.to belong_to :user }
   end
 
-  xdescribe 'scopes' do
-    before do
-    end
+  describe 'scopes' do
+    let!(:new)    { create(:streak) }
+    let!(:active) { create(:streak, :active) }
+    let!(:broken) { create(:streak, :broken) }
 
     context 'active' do
       it 'returns active streaks' do
-        expect(described_class.active.count).to eq(3)
+        expect(Streak.active.count).to eq(1)
       end
     end
 
     context 'broken' do
       it 'returns active streaks' do
-        expect(described_class.active.count).to eq(1)
+        expect(Streak.active.count).to eq(1)
       end
     end
 
     context 'newly_started' do
       it 'returns active streaks' do
-        expect(described_class.active.count).to eq(2)
+        expect(Streak.active.count).to eq(1)
       end
     end
   end
